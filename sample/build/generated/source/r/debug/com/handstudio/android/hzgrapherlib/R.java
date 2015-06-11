c06fc7ef6b853f216a6f1c811679d65e9271371a
DmitryTitorenko <d.s.titorenko@gmail.com>
2015-06-11 22:11:36 +0300

    delete more activity

--- sample/build/generated/source/r/debug/com/handstudio/android/hzgrapherlib/R.java
+++ sample/build/generated/source/r/debug/com/handstudio/android/hzgrapherlib/R.java
@@ -15,7 +15,7 @@ public final class R {
 		public static final int ic_launcher = 0x7f020001;
 	}
 	public static final class id {
-		public static final int action_settings = 0x7f08005d;
+		public static final int action_settings = 0x7f08004d;
 		public static final int layoutGraphView = 0x7f080000;
 	}
 	public static final class layout {
