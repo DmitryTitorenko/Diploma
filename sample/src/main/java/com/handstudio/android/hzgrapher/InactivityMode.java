741d4835d1522d03f8e9cbdfe48f41898431135d
Dmitry <d.s.titorenko@gmail.com>
2017-01-07 23:17:20 +0300

    Add MainAlgorithm.java

---
+++ sample/src/main/java/com/handstudio/android/hzgrapher/InactivityMode.java
@@ -0,0 +1,8 @@
+package com.handstudio.android.hzgrapher;
+
+/**
+ * Created by Dmitry Titorenko on 06.01.2017.
+ */
+
+public class InactivityMode {
+}
---
+++ sample/src/main/java/com/handstudio/android/hzgrapher/MainAlgorithm.java
@@ -0,0 +1,8 @@
+package com.handstudio.android.hzgrapher;
+
+/**
+ * Created by Dmitry Titorenko on 07.01.2017.
+ */
+
+public class MainAlgorithm {
+}
