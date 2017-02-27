a4666cb325360dd23fe4e3cb3ca2e3d60ef905cb
Dmitry <d.s.titorenko@gmail.com>
2017-02-27 20:38:54 +0300

    delete unused parameter

--- sample/src/main/java/com/handstudio/android/hzgrapher/AirMassQ.java
+++ sample/src/main/java/com/handstudio/android/hzgrapher/AirMassQ.java
@@ -6,15 +6,16 @@ package com.handstudio.android.hzgrapher;
 
 class AirMassQ {
     /**
-     * The  method used for calculation air mass.<br>
+     * The  method used for calculation air mass witch need for change temp in  1 °C .<br>
      *
+     * @param model entity witch contain all parameters
      * @return double air mass.
      */
-    public static double mathAirMassQ(Model model, int temp) {
-        double tKelvin = model.getRoomCurrentTempSingle() + 273.15;// transfer to kelvin
-        double densityP = 0.473 * (model.getAtmospherePressureP() / tKelvin);// (2.4)
+    public static double mathAirMassQ(Model model) {
+        double tKelvin = model.getRoomCurrentTempSingle() + 273.15;                          // transfer to kelvin
+        double densityP = 0.473 * (model.getAtmospherePressureP() / tKelvin);                // (2.4)
         int volumeRomm = model.getWideRoom() * model.getLengthRoom() * model.getHeightRoom();//(2.3)
-        double mass = densityP * volumeRomm; //              (2.5)
-        return (mass * model.getSpecificHeatC() * 1000*temp);//multiply in 1000 to transfer from kilojoule in joule. (2.6)
+        double mass = densityP * volumeRomm;                                                 // (2.5)
+        return (mass * model.getSpecificHeatC() * 1000);//multiply in 1000 to transfer from kilojoule in joule. (2.6)
     }
 }
