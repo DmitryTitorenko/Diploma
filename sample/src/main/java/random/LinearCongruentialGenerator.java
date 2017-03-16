c1488e157f44a65b07e0391e2e623c97a782e439
Dmitry <d.s.titorenko@gmail.com>
2017-03-16 23:28:26 +0300

    update LinearCongruentialGenerator()

--- sample/src/main/java/random/LinearCongruentialGenerator.java
+++ sample/src/main/java/random/LinearCongruentialGenerator.java
@@ -9,12 +9,12 @@ package random;
 
 public class LinearCongruentialGenerator {
 
-    // IllegalArgumentException messages
-    private static final String BadBound = "bound must be positive";
+    //first seed
     private static long seed = System.nanoTime();
     private static final long multiplier = 25214903917L;
     private static final long addend = 11;
     private static final long mask = (1L << 48) - 1;
+    private static final double DOUBLE_UNIT = 0x1.0p-53; // 1.0 / (1L << 53)
 
     /**
      * Generates the next pseudorandom number use Linear Congruential Generator.
@@ -29,29 +29,72 @@ public class LinearCongruentialGenerator {
     }
 
     /**
-     * Returns a pseudorandom, uniformly distributed {@code int} value
-     * between 0 (inclusive) and the specified value (exclusive).
+     * Returns the next pseudorandom, uniformly distributed
+     *  value between  0.0 and
+     * 1.0 from this random number generator's sequence.
      *
-     * @param bound the upper bound (exclusive).  Must be positive.
-     * @return the next pseudorandom, uniformly distributed
-     * value between zero (inclusive) and  bound (exclusive)
-     * from this random number generator's sequence
-     * @throws IllegalArgumentException if bound is not positive
+     * @return the next pseudorandom, uniformly distributed  double
+     * value between  0.0 and  1.0 from this
+     * random number generator's sequence
      */
-    private static int nextInt(int bound) {
-        if (bound <= 0)
-            throw new IllegalArgumentException(BadBound);
-        int r = next(31);
-        int m = bound - 1;
-        if ((bound & m) == 0)  // i.e., bound is a power of 2
-            r = (int) ((bound * (long) r) >> 31);
-        else {
-            r = r % bound;
+    public static double nextDouble() {
+        return (((long) (next(26)) << 27) + next(27)) * DOUBLE_UNIT;
+    }
+
+
+    private static double nextNextGaussian;
+    private static boolean haveNextNextGaussian = false;
+
+    /**
+     * Returns the next pseudorandom, Gaussian ("normally") distributed
+     *  value with mean and standard
+     * deviation 1.0 from this random number generator's sequence.
+     *
+     * @return the next pseudorandom, Gaussian ("normally") distributed
+     *  double value with mean  0.0 and
+     * standard deviation 1.0 from this random number
+     * generator's sequence
+     */
+    synchronized static public double nextGaussian() {
+        // See Knuth, ACP, Section 3.4.1 Algorithm C.
+        if (haveNextNextGaussian) {
+            haveNextNextGaussian = false;
+            return nextNextGaussian;
+        } else {
+            double v1, v2, s;
+            do {
+                v1 = 2 * nextDouble() - 1; // between -1 and 1
+                v2 = 2 * nextDouble() - 1; // between -1 and 1
+                s = v1 * v1 + v2 * v2;
+            } while (s >= 1 || s == 0);
+            double multiplier = StrictMath.sqrt(-2 * StrictMath.log(s) / s);
+            nextNextGaussian = v2 * multiplier;
+            haveNextNextGaussian = true;
+            return v1 * multiplier;
         }
-        return r;
     }
 
     public static int setRange(int min, int max) {
-        return nextInt((max - min) + 1) + min;
+        double next = getRandomFrom3Sigma();
+
+        if (next > 1) {
+            setRange(min, max);
+        }
+        return (int) (next * ((max + min)/2)/3 )+(max + min)/2;
+    }
+
+    /**
+     * Don't use value that > OR < then 3Sigma
+     * @return the next pseudorandom, Gaussian ("normally") distributed
+     *  double value with mean  0.0 and
+     * standard deviation 1.0 from this random number
+     * generator's sequence witch don't surpass 3 sigma
+     */
+    private static double getRandomFrom3Sigma() {
+        double next = nextGaussian();
+        if (next < -3 || next > 3) {
+            getRandomFrom3Sigma();
+        }
+        return next;
     }
 }
